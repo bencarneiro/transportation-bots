24761f0dce081ffcc03a6176ea09acbb28113b27
bencarneiro <bencarneiro@gmail.com>
2023-04-17 13:32:05 -0500

    add migration, model, and management command to store vision zero crash data in database

---
+++ app/views/management/commands/add_vision_zero_data_to_database.py
@@ -0,0 +1,264 @@
+
+
+import requests
+from django.core.management.base import BaseCommand
+from views.models import Crash
+
+
+class Command(BaseCommand):
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('start_block', type=int, help='scrape transactions starting with this Block #')
+    #     parser.add_argument('end_block', type=int, help='Stop Scraping Transactions when this block # is reached')
+
+    def handle(self, *args, **kwargs):
+        offset = 1000
+        keep_going = True
+        while keep_going:
+            vision_zero_data_api_url =f"https://data.austintexas.gov/resource/y2wy-tgr5.json?$order=crash_date%20DESC&$offset={offset}"
+
+            response = requests.get(vision_zero_data_api_url)
+
+            for incident in response.json():
+                print(incident)
+                crash_id = incident['crash_id']
+                crash_fatal_fl = False
+                if "crash_fatal_fl" in incident and incident['crash_fatal_fl'] == "Y":
+                    crash_fatal_fl = True
+                crash_date = incident['crash_date']
+                case_id = None
+                if "case_id" in incident:
+                    case_id = incident['case_id']
+                rpt_latitude = None
+                if "rpt_latitude" in incident:
+                    rpt_latitude = float(incident['rpt_latitude'])
+                rpt_longitude = None
+                if "rpt_longitude" in incident:
+                    rpt_longitude = float(incident['rpt_longitude'])
+                rpt_block_num = None
+                if "rpt_block_num" in incident:
+                    rpt_block_num = incident['rpt_block_num']
+                rpt_street_pfx = None
+                if "rpt_street_pfx" in incident:
+                    rpt_street_pfx = incident['rpt_street_pfx']
+                rpt_street_name = None
+                if "rpt_street_name" in incident:
+                    rpt_street_name = incident['rpt_street_name']
+                rpt_street_sfx = None
+                if "rpt_street_sfx" in incident:
+                    rpt_street_sfx = incident['rpt_street_sfx']
+                crash_speed_limit = None
+                if "crash_speed_limit" in incident:
+                    crash_speed_limit = incident['crash_speed_limit']
+                road_constr_zone_fl = False
+                if "road_constr_zone_fl" in incident and incident['road_constr_zone_fl'] == "Y":
+                    road_constr_zone_fl = True
+                latitude = None
+                if "latitude" in incident:
+                    latitude = float(incident['latitude'])
+                longitude = None
+                if "longitude" in incident:
+                    longitude = float(incident['longitude'])
+                street_name = None
+                if "street_name" in incident:
+                    street_name = incident['street_name']
+                street_nbr = None
+                if "street_nbr" in incident:
+                    street_nbr = incident['street_nbr']
+                street_name_2 = None
+                if "street_name_2" in incident:
+                    street_name_2 = incident['street_name_2']
+                street_nbr_2 = None
+                if "street_nbr_2" in incident:
+                    street_nbr_2 = incident['street_nbr_2']
+                crash_sev_id = None
+                if "crash_sev_id" in incident:
+                    crash_sev_id = int(incident['crash_sev_id'])
+                # sus_serious_injry_cnt
+                sus_serious_injry_cnt = 0
+                if "sus_serious_injry_cnt" in incident:
+                    sus_serious_injry_cnt = int(incident['sus_serious_injry_cnt'])
+                # nonincap_injry_cnt
+                nonincap_injry_cnt = 0
+                if "nonincap_injry_cnt" in incident:
+                    nonincap_injry_cnt = int(incident['nonincap_injry_cnt'])
+                # poss_injry_cnt
+                poss_injry_cnt = 0
+                if "poss_injry_cnt" in incident:
+                    poss_injry_cnt = int(incident['poss_injry_cnt'])
+                # non_injry_cnt
+                non_injry_cnt = 0
+                if "non_injry_cnt" in incident:
+                    non_injry_cnt = int(incident['non_injry_cnt'])
+                # unkn_injry_cnt
+                unkn_injry_cnt = 0
+                if "unkn_injry_cnt" in incident:
+                    unkn_injry_cnt = int(incident['unkn_injry_cnt'])
+                # tot_injry_cnt
+                tot_injry_cnt = 0
+                if "tot_injry_cnt" in incident:
+                    tot_injry_cnt = int(incident['tot_injry_cnt'])
+                # death_cnt
+                death_cnt = 0
+                if "death_cnt" in incident:
+                    death_cnt = int(incident['death_cnt'])
+                # contrib_factr_p1_id
+                contrib_factr_p1_id = None
+                if "contrib_factr_p1_id" in incident:
+                    contrib_factr_p1_id = int(incident['contrib_factr_p1_id'])
+                # contrib_factr_p2_id
+                contrib_factr_p2_id = None
+                if "contrib_factr_p2_id" in incident:
+                    contrib_factr_p2_id = int(incident['contrib_factr_p2_id'])
+                # units_involved
+                units_involved = None
+                if "units_involved" in incident:
+                    units_involved = incident['units_involved']
+
+                # atd_mode_category_metadata
+                atd_mode_category_metadata = None
+                if "atd_mode_category_metadata" in incident:
+                    atd_mode_category_metadata = incident['atd_mode_category_metadata']
+                
+                # pedestrian_fl
+                pedestrian_fl = False
+                if "pedestrian_fl" in incident and incident['pedestrian_fl'] == "Y":
+                    pedestrian_fl = True
+                # motor_vehicle_fl
+                motor_vehicle_fl = False
+                if "motor_vehicle_fl" in incident and incident['motor_vehicle_fl'] == "Y":
+                    motor_vehicle_fl = True
+                # motorcycle_fl
+                motorcycle_fl = False
+                if "motorcycle_fl" in incident and incident['motorcycle_fl'] == "Y":
+                    motorcycle_fl = True
+                # bicycle_fl
+                bicycle_fl = False
+                if "bicycle_fl" in incident and incident['bicycle_fl'] == "Y":
+                    bicycle_fl = True
+                # other_fl
+                other_fl = False
+                if "other_fl" in incident and incident['other_fl'] == "Y":
+                    other_fl = True
+
+                # point
+                point = None
+                if "point" in incident:
+                    point = incident['point']
+                # apd_confirmed_fatality
+                apd_confirmed_fatality = False
+                if "apd_confirmed_fatality" in incident and incident['apd_confirmed_fatality'] == "Y":
+                    apd_confirmed_fatality = True
+
+                # apd_confirmed_death_count
+                apd_confirmed_death_count = 0
+                if "apd_confirmed_death_count" in incident:
+                    apd_confirmed_death_count = int(incident['apd_confirmed_death_count'])
+                # motor_vehicle_death_count
+                motor_vehicle_death_count = 0
+                if "motor_vehicle_death_count" in incident:
+                    motor_vehicle_death_count = int(incident['motor_vehicle_death_count'])
+                # motor_vehicle_serious_injury_count
+                motor_vehicle_serious_injury_count = 0
+                if "motor_vehicle_serious_injury_count" in incident:
+                    motor_vehicle_serious_injury_count = int(incident['motor_vehicle_serious_injury_count'])
+                # bicycle_death_count
+                bicycle_death_count = 0
+                if "bicycle_death_count" in incident:
+                    bicycle_death_count = int(incident['bicycle_death_count'])
+                # bicycle_serious_injury_count
+                bicycle_serious_injury_count = 0
+                if "bicycle_serious_injury_count" in incident:
+                    bicycle_serious_injury_count = int(incident['bicycle_serious_injury_count'])
+                # pedestrian_death_count
+                pedestrian_death_count = 0
+                if "pedestrian_death_count" in incident:
+                    pedestrian_death_count = int(incident['pedestrian_death_count'])
+                # pedestrian_serious_injury_count
+                pedestrian_serious_injury_count = 0
+                if "pedestrian_serious_injury_count" in incident:
+                    pedestrian_serious_injury_count = int(incident['pedestrian_serious_injury_count'])
+                # motorcycle_death_count
+                motorcycle_death_count = 0
+                if "motorcycle_death_count" in incident:
+                    motorcycle_death_count = int(incident['motorcycle_death_count'])
+                # motorcycle_serious_injury_count
+                motorcycle_serious_injury_count = 0
+                if "motorcycle_serious_injury_count" in incident:
+                    motorcycle_serious_injury_count = int(incident['motorcycle_serious_injury_count'])
+                # other_death_count
+                other_death_count = 0
+                if "other_death_count" in incident:
+                    other_death_count = int(incident['other_death_count'])
+                # other_serious_injury_count
+                other_serious_injury_count = 0
+                if "other_serious_injury_count" in incident:
+                    other_serious_injury_count = int(incident['other_serious_injury_count'])
+                # onsys_fl
+                onsys_fl = False
+                if "onsys_fl" in incident and incident['onsys_fl'] == "Y":
+                    onsys_fl = True
+                # private_dr_fl 
+                private_dr_fl = False
+                if "private_dr_fl" in incident and incident['private_dr_fl'] == "Y":
+                    private_dr_fl = True
+
+                new_crash = Crash(
+                    crash_id=crash_id,
+                    crash_fatal_fl=crash_fatal_fl,
+                    crash_date=crash_date,
+                    case_id=case_id,
+                    rpt_latitude=rpt_latitude,
+                    rpt_longitude=rpt_longitude,
+                    rpt_block_num=rpt_block_num,
+                    rpt_street_pfx=rpt_street_pfx,
+                    rpt_street_name=rpt_street_name,
+                    rpt_street_sfx=rpt_street_sfx,
+                    crash_speed_limit=crash_speed_limit,
+                    road_constr_zone_fl=road_constr_zone_fl,
+                    latitude=latitude,
+                    longitude=longitude,
+                    street_name=street_name,
+                    street_nbr=street_nbr,
+                    street_name_2=street_name_2,
+                    street_nbr_2=street_nbr_2,
+                    crash_sev_id=crash_sev_id,
+                    sus_serious_injry_cnt=sus_serious_injry_cnt,
+                    nonincap_injry_cnt=nonincap_injry_cnt,
+                    poss_injry_cnt=poss_injry_cnt,
+                    non_injry_cnt=non_injry_cnt,
+                    unkn_injry_cnt=unkn_injry_cnt,
+                    tot_injry_cnt=tot_injry_cnt,
+                    death_cnt=death_cnt,
+                    contrib_factr_p1_id=contrib_factr_p1_id,
+                    contrib_factr_p2_id=contrib_factr_p2_id,
+                    units_involved=units_involved,
+                    atd_mode_category_metadata=atd_mode_category_metadata,
+                    pedestrian_fl=pedestrian_fl,
+                    motor_vehicle_fl=motor_vehicle_fl,
+                    motorcycle_fl=motorcycle_fl,
+                    bicycle_fl=bicycle_fl,
+                    other_fl=other_fl,
+                    point=point,
+                    apd_confirmed_fatality=apd_confirmed_fatality,
+                    apd_confirmed_death_count=apd_confirmed_death_count,
+                    motor_vehicle_death_count=motor_vehicle_death_count,
+                    motor_vehicle_serious_injury_count=motor_vehicle_serious_injury_count,
+                    bicycle_death_count=bicycle_death_count,
+                    bicycle_serious_injury_count=bicycle_serious_injury_count,
+                    pedestrian_death_count=pedestrian_death_count,
+                    pedestrian_serious_injury_count=pedestrian_serious_injury_count,
+                    motorcycle_death_count=motorcycle_death_count,
+                    motorcycle_serious_injury_count=motorcycle_serious_injury_count,
+                    other_death_count=other_death_count,
+                    other_serious_injury_count=other_serious_injury_count,
+                    onsys_fl=onsys_fl,
+                    private_dr_fl=private_dr_fl
+                )
+                new_crash.save()
+                print(f"SUCCESSFULLY SAVED CRASH_ID {incident['crash_id']}")
+                
+            if len(response.json()) < 1000:
+                keep_going = False
+            else: 
+                offset += 1000
--- app/views/models.py
+++ app/views/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import PositiveBigIntegerField, BigIntegerField
 
 # Create your models here.
 
@@ -18,3 +19,61 @@ class TrafficReport(models.Model):
     class Meta:
         managed = True
         db_table = 'traffic_report'
+
+
+class Crash(models.Model):
+
+    crash_id = models.PositiveBigIntegerField(primary_key=True)
+    crash_fatal_fl = models.BooleanField(default=False) # this one needs to be converted from N / Y
+    crash_date = models.DateTimeField(blank=False, null=False)
+    case_id = models.CharField(max_length=256, null=True)
+    rpt_latitude = models.FloatField(null=True)
+    rpt_longitude = models.FloatField(null=True)
+    rpt_block_num = models.CharField(max_length=256, null=True)
+    rpt_street_pfx = models.CharField(max_length=256, null=True)
+    rpt_street_name = models.CharField(max_length=256, null=True)
+    rpt_street_sfx = models.CharField(max_length=256, null=True)
+    crash_speed_limit = models.IntegerField(null=True)
+    road_constr_zone_fl = models.BooleanField(default=False, null=True)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    street_name = models.CharField(max_length=256, null=True)
+    street_nbr = models.CharField(max_length=256, null=True)
+    street_name_2 = models.CharField(max_length=256, null=True)
+    street_nbr_2 = models.CharField(max_length=256, null=True)
+    crash_sev_id = models.PositiveIntegerField(null=True)
+    sus_serious_injry_cnt = models.PositiveIntegerField(default=0)
+    nonincap_injry_cnt = models.PositiveIntegerField(default=0)
+    poss_injry_cnt = models.PositiveIntegerField(default=0)
+    non_injry_cnt = models.PositiveIntegerField(default=0)
+    unkn_injry_cnt = models.PositiveIntegerField(default=0)
+    tot_injry_cnt = models.PositiveIntegerField(default=0)
+    death_cnt = models.PositiveIntegerField(default=0)
+    contrib_factr_p1_id = models.PositiveIntegerField(null=True)
+    contrib_factr_p2_id = models.PositiveIntegerField(null=True)
+    units_involved = models.CharField(max_length=512, null=True)
+    atd_mode_category_metadata = models.TextField(null=True)
+    pedestrian_fl = models.BooleanField(default=False)
+    motor_vehicle_fl = models.BooleanField(default=False)
+    motorcycle_fl = models.BooleanField(default=False)
+    bicycle_fl = models.BooleanField(default=False)
+    other_fl = models.BooleanField(default=False)
+    point = models.CharField(max_length=256, null=True)
+    apd_confirmed_fatality = models.BooleanField(default=False) # this one needs to be converted from N / Y
+    apd_confirmed_death_count = models.PositiveIntegerField(default=0)
+    motor_vehicle_death_count = models.PositiveIntegerField(default=0)
+    motor_vehicle_serious_injury_count = models.PositiveIntegerField(default=0)
+    bicycle_death_count = models.PositiveIntegerField(default=0)
+    bicycle_serious_injury_count = models.PositiveIntegerField(default=0)
+    pedestrian_death_count = models.PositiveIntegerField(default=0)
+    pedestrian_serious_injury_count = models.PositiveIntegerField(default=0)
+    motorcycle_death_count = models.PositiveIntegerField(default=0)
+    motorcycle_serious_injury_count = models.PositiveIntegerField(default=0)
+    other_death_count = models.PositiveIntegerField(default=0)
+    other_serious_injury_count = models.PositiveIntegerField(default=0)
+    onsys_fl = models.BooleanField(default=False)
+    private_dr_fl = models.BooleanField(default=False)
+        
+    class Meta:
+        managed = True
+        db_table = 'crash'
