f8042fc8d0499fc5961e9eedf6bb7a580b06c664
bencarneiro <bencarneiro@gmail.com>
2023-05-15 12:40:50 -0500

    fix year formatting on the performance by service chart

--- frontend/src/PerformanceByService.js
+++ frontend/src/PerformanceByService.js
@@ -25,7 +25,7 @@ const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
     return (
       <div className="custom-tooltip" style={{ backgroundColor: "white", fontSize: "16px", margin: "10px" }}>
-        <p className="label">{`Year: ${payload[0].payload.year}`}</p>
+        <h3 className="label">{`${payload[0].payload.year}`}</h3>
         {payload[0].payload.do > 0 && (
           <p className="label">{`Directly Operated: ${new Intl.NumberFormat("en-US", {
             notation: "compact",
