21a7afc767a17a9ac924228c13d7d549928b4ddf
bencarneiro <bencarneiro@gmail.com>
2023-05-23 09:49:20 -0500

    add some blog stuff

--- frontend/src/Blog.js
+++ frontend/src/Blog.js
@@ -1,4 +1,4 @@
-
+import './App.css';
 import axios from "axios"
 import React, { useState } from 'react';
 
@@ -7,7 +7,44 @@ function Blog() {
 
 
   return (
-    <div>More to come here</div>
+    <div>
+    <h1>The Train To Nowhere</h1>
+    <h2>How to Nuke Your Transit System as Quickly as Possible</h2>
+    <div>By Ben Carneiro</div>
+    <br></br><br></br><br></br>
+    <div>
+        I need to be clear about one thing. I fucking love trains. 
+        <br></br><br></br>
+        Electric cars don't solve climate change. 
+        <br></br><br></br>
+        
+        Self-driving vehicles don't solve congestion.
+        <br></br><br></br>
+        
+        Cars will essentially have no role in saving the world from climate catastrophe. 
+        <br></br><br></br>
+
+        Public Transit in the United States is under constant attack from private interests (link elon and caltrain), 
+        as well as politicians at all levels of government
+        <br></br><br></br>
+        [show revenue growth in Austin]
+        <br></br><br></br>
+        [Show Rail Expenses as a percentage of Operating Expenses]
+        <br></br><br></br>
+        [Show Rail Expenses as a percentage of capital expenses]
+        <br></br><br></br>
+
+        <br></br><br></br>
+
+        <br></br><br></br>
+
+    </div>
+    <br></br><br></br><br></br>
+
+
+    <footer></footer>
+    
+    </div>
   );
 }
 
