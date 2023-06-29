b5395df3fc45c68b7abb61abe4b1bf4a0872299d
bencarneiro <bencarneiro@gmail.com>
2023-06-29 10:07:51 -0500

    keep writing queries to sketch out how to access transit data

--- sql/capmetro.sql
+++ sql/capmetro.sql
@@ -29,4 +29,25 @@ and departure_time < "1970-01-01 10:50:00"
 and stop_times.stop_id in (
 SELECT id FROM stops
 where SQRT(POWER(ABS(longitude - (-97.72022063254802)), 2) + POWER(ABS(latitude - (30.314065874794284)), 2)) < .005
+order by SQRT(POWER(ABS(longitude - (-97.72022063254802)), 2) + POWER(ABS(latitude - (30.314065874794284)), 2)) ASC);
+
+
+select time(stop_times.departure_time) as departs,
+trip_headsign,
+stops.stop_id,
+trips.shape_id,
+trips.trip_id
+from stop_times
+left join trips on trips.id = stop_times.trip_id
+left join routes on trips.route_id = routes.id
+LEFT JOIN stops on stop_times.stop_id = stops.id
+where stop_times.trip_id in
+(select id from trips where service_id in 
+(select service_id from calendar_dates where date = 20230629))
+and departure_time > "1970-01-01 09:40:00"
+and departure_time < "1970-01-01 10:50:00"
+and stop_times.stop_id in (
+SELECT id FROM stops
+where SQRT(POWER(ABS(longitude - (-97.72022063254802)), 2) + POWER(ABS(latitude - (30.314065874794284)), 2)) < .005
 order by SQRT(POWER(ABS(longitude - (-97.72022063254802)), 2) + POWER(ABS(latitude - (30.314065874794284)), 2)) ASC)
+order by stop_times.departure_time;
