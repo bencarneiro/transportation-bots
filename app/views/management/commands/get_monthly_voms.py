2f4892819df86f818a7f4f096700cc78336556bb
bencarneiro <bencarneiro@gmail.com>
2024-11-29 15:32:04 -0600

    update the scripts which pull in the monthly ridership data

--- app/views/management/commands/get_monthly_voms.py
+++ app/views/management/commands/get_monthly_voms.py
@@ -7,36 +7,39 @@ import datetime
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        MonthlyVehiclesOperatedMaximumService.objects.all().delete()
         dates = []
-        for year in range(2002,2024):
+        for year in range(2002,2025):
         #     print(z)
             for month in range(12):
         #         print(x + 1)
                 date = str((month + 1)) + "/" + str(year)
-                if year == 2023 and month == 3:
+                if year == 2024 and month == 9:
                     break
                 dates += [date]
-        voms = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2023-05/March%202023%20Complete%20Monthly%20Ridership%20%28with%20adjustments%20and%20estimates%29.xlsx', sheet_name="VOMS", engine="openpyxl")
+        voms = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2024-11/September%202024%20Complete%20Monthly%20Ridership%20%28with%20adjustments%20and%20estimates%29_241101.xlsx', sheet_name="VOMS", engine="openpyxl")
         voms[dates] = voms[dates].fillna(0)
-        voms[['UZA', 'NTD ID']] = voms[['UZA', 'NTD ID']].fillna(0)
+        voms[['UACE CD', 'NTD ID']] = voms[['UACE CD', 'NTD ID']].fillna(0)
 
         for x in voms.index: 
+            print(x)
             transit_agencies = TransitAgency.objects.filter(ntd_id=voms['NTD ID'][x], legacy_ntd_id=voms['Legacy NTD ID'][x])
             if len(transit_agencies) < 1:
                 transit_agency = TransitAgency(
                     ntd_id = voms['NTD ID'][x],
                     legacy_ntd_id = voms['Legacy NTD ID'][x],
                     agency_name = voms['Agency'][x],
-                    agency_status = voms['Status'][x],
+                    # agency_status = voms['Status'][x],
                     reporter_type = voms['Reporter Type'][x],
                     uza_name = voms['UZA Name'][x],
-                    uza = voms['UZA'][x]
+                    uza = voms['UACE CD'][x]
                 )
                 transit_agency.save()
             else:
                 transit_agency = transit_agencies[0]
             # print(x)
             # print(voms[year][x])
+            new_data = []
             for date in dates:
                 date_split = date.split("/")
                 month = date_split[0]
@@ -49,4 +52,6 @@ class Command(BaseCommand):
                     month = int(month),
                     date =  datetime.datetime(day=1,month=int(month),year=int(year)),
                     voms = voms[date][x]
-                ).save()
+                )
+                new_data += [new_transit_expense]
+            MonthlyVehiclesOperatedMaximumService.objects.bulk_create(new_data)
--- app/views/management/commands/get_monthly_vrm.py
+++ app/views/management/commands/get_monthly_vrm.py
@@ -7,36 +7,40 @@ import datetime
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+
+        MonthlyVehicleRevenueMiles.objects.all().delete()
         dates = []
-        for year in range(2002,2024):
+        for year in range(2002,2025):
         #     print(z)
             for month in range(12):
         #         print(x + 1)
                 date = str((month + 1)) + "/" + str(year)
-                if year == 2023 and month == 3:
+                if year == 2024 and month == 9:
                     break
                 dates += [date]
-        vrm = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2023-05/March%202023%20Complete%20Monthly%20Ridership%20%28with%20adjustments%20and%20estimates%29.xlsx', sheet_name="VRM", engine="openpyxl")
+        vrm = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2024-11/September%202024%20Complete%20Monthly%20Ridership%20%28with%20adjustments%20and%20estimates%29_241101.xlsx', sheet_name="VRM", engine="openpyxl")
         vrm[dates] = vrm[dates].fillna(0)
-        vrm[['UZA', 'NTD ID']] = vrm[['UZA', 'NTD ID']].fillna(0)
+        vrm[['UACE CD', 'NTD ID']] = vrm[['UACE CD', 'NTD ID']].fillna(0)
 
         for x in vrm.index: 
+            print(x)
             transit_agencies = TransitAgency.objects.filter(ntd_id=vrm['NTD ID'][x], legacy_ntd_id=vrm['Legacy NTD ID'][x])
             if len(transit_agencies) < 1:
                 transit_agency = TransitAgency(
                     ntd_id = vrm['NTD ID'][x],
                     legacy_ntd_id = vrm['Legacy NTD ID'][x],
                     agency_name = vrm['Agency'][x],
-                    agency_status = vrm['Status'][x],
+                    # agency_status = vrm['Status'][x],
                     reporter_type = vrm['Reporter Type'][x],
                     uza_name = vrm['UZA Name'][x],
-                    uza = vrm['UZA'][x]
+                    uza = vrm['UACE CD'][x]
                 )
                 transit_agency.save()
             else:
                 transit_agency = transit_agencies[0]
             # print(x)
             # print(vrm[year][x])
+            new_data = []
             for date in dates:
                 date_split = date.split("/")
                 month = date_split[0]
@@ -49,4 +53,6 @@ class Command(BaseCommand):
                     month = int(month),
                     date =  datetime.datetime(day=1,month=int(month),year=int(year)),
                     vrm = vrm[date][x]
-                ).save()
+                )
+                new_data += [new_transit_expense]
+            MonthlyVehicleRevenueMiles.objects.bulk_create(new_data)
