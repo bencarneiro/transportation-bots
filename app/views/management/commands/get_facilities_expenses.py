67ff8e0941027256e90d3d37ed904ccbae6bf1ca
bencarneiro <bencarneiro@gmail.com>
2024-11-29 14:36:06 -0600

    fix scripts to read in updated capital expense data

--- app/views/management/commands/get_facilities_expenses.py
+++ app/views/management/commands/get_facilities_expenses.py
@@ -7,42 +7,45 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         years = []
-        for x in range(1992,2022):
+        for x in range(1992,2024):
             years += [str(x)]
-        expense_facilities = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2022-10/TS3.1%20Capital%20Expenditures%20Time%20Series_0.xlsx', sheet_name="Facilities", engine="openpyxl")
-        expense_facilities[years] = expense_facilities[years].fillna(0)
-        expense_facilities[['UZA', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']] = expense_facilities[['UZA', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']].fillna(0)
-        for x in expense_facilities.index: 
-            transit_agencies = TransitAgency.objects.filter(ntd_id=expense_facilities['NTD ID'][x], legacy_ntd_id=expense_facilities['Legacy NTD ID'][x])
+        expense_fc = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2024-10/2023%20TS3.1%20Capital%20Expenditures%20Time%20Series.xlsx', sheet_name="Facilities", engine="openpyxl")
+        expense_fc[years] = expense_fc[years].fillna(0)
+        expense_fc[['UACE Code', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']] = expense_fc[['UACE Code', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']].fillna(0)
+        for x in expense_fc.index: 
+            transit_agencies = TransitAgency.objects.filter(ntd_id=expense_fc['NTD ID'][x], legacy_ntd_id=expense_fc['Legacy NTD ID'][x])
             if len(transit_agencies) < 1:
                 transit_agency = TransitAgency(
-                    last_report_year=expense_facilities['Last Report Year'][x],
-                    ntd_id = expense_facilities['NTD ID'][x],
-                    legacy_ntd_id = expense_facilities['Legacy NTD ID'][x],
-                    agency_name = expense_facilities['Agency Name'][x],
-                    agency_status = expense_facilities['Agency Status'][x],
-                    reporter_type = expense_facilities['Reporter Type'][x],
-                    reporting_module = expense_facilities['Reporting Module'][x],
-                    city = expense_facilities['City'][x],
-                    state = expense_facilities['State'][x],
-                    census_year = expense_facilities['Census Year'][x],
-                    uza_name = expense_facilities['UZA Name'][x],
-                    uza = expense_facilities['UZA'][x],
-                    uza_area_sqm = expense_facilities['UZA Area SQ Miles'][x],
-                    uza_population = expense_facilities['UZA Population'][x],
-                    status_2021 = expense_facilities['2021 Status'][x],
+                    last_report_year=expense_fc['Last Report Year'][x],
+                    ntd_id = expense_fc['NTD ID'][x],
+                    legacy_ntd_id = expense_fc['Legacy NTD ID'][x],
+                    agency_name = expense_fc['Agency Name'][x],
+                    agency_status = expense_fc['Agency Status'][x],
+                    reporter_type = expense_fc['Reporter Type'][x],
+                    reporting_module = expense_fc['Reporting Module'][x],
+                    city = expense_fc['City'][x],
+                    state = expense_fc['State'][x],
+                    census_year = expense_fc['Census Year'][x],
+                    uza_name = expense_fc['UZA Name'][x],
+                    uza = expense_fc['UACE Code'][x],
+                    uza_area_sqm = expense_fc['UZA Area SQ Miles'][x],
+                    uza_population = expense_fc['UZA Population'][x],
+                    # status_2021 = expense_fc['Agency Status'][x],
                 )
                 transit_agency.save()
             else:
                 transit_agency = transit_agencies[0]
             print(x)
-            # print(expense_facilities[year][x])
+            # print(expense_fc[year][x])
+            expenses = []
             for year in years:
                 new_transit_expense = TransitExpense(
                     transit_agency=transit_agency,
-                    mode_id = expense_facilities['Mode'][x],
+                    mode_id = expense_fc['Mode'][x],
                     service_id = "DO",
                     year_id = int(year),
                     expense_type_id = "FC",
-                    expense = expense_facilities[year][x]
-                ).save()
+                    expense = expense_fc[year][x]
+                )
+                expenses += [new_transit_expense]
+            TransitExpense.objects.bulk_create(expenses)
--- app/views/management/commands/get_rolling_stock_expense.py
+++ app/views/management/commands/get_rolling_stock_expense.py
@@ -7,42 +7,45 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         years = []
-        for x in range(1992,2022):
+        for x in range(1992,2024):
             years += [str(x)]
-        expense_ga = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2022-10/TS3.1%20Capital%20Expenditures%20Time%20Series_0.xlsx', sheet_name="Rolling Stock", engine="openpyxl")
-        expense_ga[years] = expense_ga[years].fillna(0)
-        expense_ga[['UZA', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']] = expense_ga[['UZA', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']].fillna(0)
-        for x in expense_ga.index: 
-            transit_agencies = TransitAgency.objects.filter(ntd_id=expense_ga['NTD ID'][x], legacy_ntd_id=expense_ga['Legacy NTD ID'][x])
+        expense_rs = pd.read_excel('https://www.transit.dot.gov/sites/fta.dot.gov/files/2024-10/2023%20TS3.1%20Capital%20Expenditures%20Time%20Series.xlsx', sheet_name="Rolling Stock", engine="openpyxl")
+        expense_rs[years] = expense_rs[years].fillna(0)
+        expense_rs[['UACE Code', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']] = expense_rs[['UACE Code', 'UZA Area SQ Miles', 'UZA Population', 'NTD ID']].fillna(0)
+        for x in expense_rs.index: 
+            transit_agencies = TransitAgency.objects.filter(ntd_id=expense_rs['NTD ID'][x], legacy_ntd_id=expense_rs['Legacy NTD ID'][x])
             if len(transit_agencies) < 1:
                 transit_agency = TransitAgency(
-                    last_report_year=expense_ga['Last Report Year'][x],
-                    ntd_id = expense_ga['NTD ID'][x],
-                    legacy_ntd_id = expense_ga['Legacy NTD ID'][x],
-                    agency_name = expense_ga['Agency Name'][x],
-                    agency_status = expense_ga['Agency Status'][x],
-                    reporter_type = expense_ga['Reporter Type'][x],
-                    reporting_module = expense_ga['Reporting Module'][x],
-                    city = expense_ga['City'][x],
-                    state = expense_ga['State'][x],
-                    census_year = expense_ga['Census Year'][x],
-                    uza_name = expense_ga['UZA Name'][x],
-                    uza = expense_ga['UZA'][x],
-                    uza_area_sqm = expense_ga['UZA Area SQ Miles'][x],
-                    uza_population = expense_ga['UZA Population'][x],
-                    status_2021 = expense_ga['2021 Status'][x],
+                    last_report_year=expense_rs['Last Report Year'][x],
+                    ntd_id = expense_rs['NTD ID'][x],
+                    legacy_ntd_id = expense_rs['Legacy NTD ID'][x],
+                    agency_name = expense_rs['Agency Name'][x],
+                    agency_status = expense_rs['Agency Status'][x],
+                    reporter_type = expense_rs['Reporter Type'][x],
+                    reporting_module = expense_rs['Reporting Module'][x],
+                    city = expense_rs['City'][x],
+                    state = expense_rs['State'][x],
+                    census_year = expense_rs['Census Year'][x],
+                    uza_name = expense_rs['UZA Name'][x],
+                    uza = expense_rs['UACE Code'][x],
+                    uza_area_sqm = expense_rs['UZA Area SQ Miles'][x],
+                    uza_population = expense_rs['UZA Population'][x],
+                    # status_2021 = expense_rs['Agency Status'][x],
                 )
                 transit_agency.save()
             else:
                 transit_agency = transit_agencies[0]
             print(x)
-            # print(expense_ga[year][x])
+            # print(expense_rs[year][x])
+            expenses = []
             for year in years:
                 new_transit_expense = TransitExpense(
                     transit_agency=transit_agency,
-                    mode_id = expense_ga['Mode'][x],
+                    mode_id = expense_rs['Mode'][x],
                     service_id = "DO",
                     year_id = int(year),
                     expense_type_id = "RS",
-                    expense = expense_ga[year][x]
-                ).save()
+                    expense = expense_rs[year][x]
+                )
+                expenses += [new_transit_expense]
+            TransitExpense.objects.bulk_create(expenses)
