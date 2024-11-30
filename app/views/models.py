0e0a1d4c7ca363eb4c71e01bc09ba6301d114af3
bencarneiro <bencarneiro@gmail.com>
2024-11-30 09:01:06 -0600

    add a migration to make GTFS data have nullable foreign key to transit agency- to allow for this big data update

--- app/views/models.py
+++ app/views/models.py
@@ -310,7 +310,7 @@ class MonthlyVehicleRevenueHours(models.Model):
 class Shapes(models.Model):
     id = models.AutoField(primary_key=True)
     shape_id = models.PositiveIntegerField(null=False)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     shape_pt_lat = models.FloatField(null=False)
     shape_pt_lon = models.FloatField(null=False)
     shape_pt_sequence = models.PositiveIntegerField(null=True)
@@ -323,7 +323,7 @@ class Shapes(models.Model):
 class CalendarDates(models.Model):
 
     id = models.AutoField(primary_key=True)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     date = models.DateField(null=False)
     service_id = models.CharField(null=False, max_length=64)
     exception_type = models.IntegerField(default=1)
@@ -337,7 +337,7 @@ class Routes(models.Model):
 
     id = models.AutoField(primary_key=True)
     route_id = models.PositiveBigIntegerField(null=False)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     route_short_name = models.CharField(null=True, blank=True, max_length=64)
     route_long_name = models.CharField(null=True, blank=True, max_length=256)
     route_type = models.PositiveSmallIntegerField(null=True)
@@ -354,7 +354,7 @@ class Trips(models.Model):
 
     id = models.AutoField(primary_key=True)
     trip_id = models.CharField(null=False, max_length=128)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     route = models.ForeignKey(Routes, on_delete=models.DO_NOTHING)
     service_id = models.CharField(max_length=64)
     trip_headsign  = models.CharField(max_length=64)
@@ -373,7 +373,7 @@ class Trips(models.Model):
 class Stops(models.Model):
     id = models.AutoField(primary_key=True)
     stop_id = models.PositiveIntegerField(null=False)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     at_street = models.CharField(max_length=128)
     corner_placement = models.CharField(max_length=16)
     heading = models.PositiveSmallIntegerField(null=True, blank=True)
@@ -396,7 +396,7 @@ class Stops(models.Model):
 
 class StopTimes(models.Model):
     id = models.AutoField(primary_key=True)
-    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING)
+    transit_agency = models.ForeignKey(TransitAgency, on_delete=models.DO_NOTHING, null=True)
     trip = models.ForeignKey(Trips, on_delete=models.DO_NOTHING)
     stop = models.ForeignKey(Stops, on_delete=models.DO_NOTHING)
     arrival_time = models.DateTimeField(null=True, blank=True)
